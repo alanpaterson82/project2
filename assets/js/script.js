a3c716ce2fae61f77c70afc176bbf12a806f66a6
alanpaterson82 <>
2023-05-08 09:56:26 +0000

    further js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,31 @@ function showResults(){
     // keep track of user's answers
     let numCorrect = 0;
 
-    
+    // for each question...
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked` ;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+        
+        // if answer is correct
+    if(userAnswer === currentQuestion.correctAnswer){
+            // add to the number of correct answers
+            numCorrect++;
+
+            // color the answers green
+            answerContainers[questionNumber].style.color = 'lightgreen';
+    }
+        // if answer is wrong or blank
+    else{
+            // color answers red
+            answerContainers[questionNumber].style.color = 'red';
+    }
+});
+
+// show number of correct answers out of total
+resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 }
 
 const quizContainer = document.getElementById("quiz");
